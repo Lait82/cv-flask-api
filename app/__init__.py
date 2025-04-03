05ba5837b849f3b9f0d83e89392e3b4261b9baf3
lait82 <manuexposito82@outlook.com>
2025-04-03 02:58:24 -0300

    Fix: el servicio del webserver ahora espera a que el servicio de base de datos inicie para arrancar. Add: Init de migraciones. Add: Readme file. TO-DO:  Agregar directorio de migraciones y crear las tablas.

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from .routes.api import blueprint as api_blueprint
+from dotenv import load_dotenv
 
 db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
+    load_dotenv()
     app.config.from_object('app.config.Config')
     
     # Inicializar la base de datos
     db.init_app(app)
+    migrate.init_app(app, db)
 
     # Registrar rutas
     app.register_blueprint(api_blueprint)
