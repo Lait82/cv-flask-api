fc82bb5c7b41e63adc4def506c6876e579285863
lait82 <manuexposito82@outlook.com>
2025-04-25 19:13:18 +0000

    update: largo de los campos de contacto.

--- app/models/Contact.py
+++ app/models/Contact.py
@@ -5,11 +5,11 @@ class Contact(BaseModel):
     __tablename__ = 'contacts'
     
     id = Column(Integer, primary_key=True)
-    name = Column(String(16))
-    email = Column(String(32))
-    url = Column(String(128))
-    company = Column(String(32))
-    contact_message = Column(Text(1024))
+    name = Column(String(64))
+    email = Column(String(64))
+    url = Column(String(256))
+    company = Column(String(64))
+    contact_message = Column(Text(2048))
 
     def __init__(self, name, email, url, company, contact_message):
         self.name = name
